e8dc4357e1d810b5073e964a869121c9f5ba07bb
M00N1C <mostafa.mcm.85@gmail.com>
2025-03-06 06:26:02 +0330

    Redesigned the game using cool features: -Added new mode (Prize Table):players get to do another move as a prize for hitting a shot. -Added AI (Hard and Easy mode): In hard mode AI decides to move based of possible previous hits.

--- Answers/BattleShip/src/main/java/BattleShip.java
+++ Answers/BattleShip/src/main/java/BattleShip.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
-/**
-  The BattleShip class manages the gameplay of the Battleship game between two players.
-  It includes methods to manage grids, turns, and check the game status.
- */
+// BattleShip by @Pv_McM
+
 public class BattleShip {
+
+    static int row;
+    static int col;
+    static int size;
+    static boolean horizontal;
+
+    static int remainParts;
+    static int hits;
+    static int misses;
+    static int totalShots;
+
+    static boolean isHit;
+    static int lastHitRow;
+    static int lastHitCol;
 
     // Grid size for the game
     static final int GRID_SIZE = 10;
@@ -24,33 +39,40 @@ public class BattleShip {
     // Scanner object for user input
     static Scanner scanner = new Scanner(System.in);
 
-    /**
-      The main method that runs the game loop.
-      It initializes the grids for both players, places ships randomly, and manages turns.
-      The game continues until one player's ships are completely sunk.
-     */
     public static void main(String[] args) {
-        // Initialize grids for both players
-        initializeGrid(player1Grid);
-        initializeGrid(player2Grid);
-        initializeGrid(player1TrackingGrid);
-        initializeGrid(player2TrackingGrid);
+        System.out.println("=======================");
+        System.out.println("=  BattleShip by McM  =");
+        System.out.println("=======================");
+        System.out.println();
+        System.out.println("Choose your option:");
+        System.out.println("1.PvP (Normal Match)");
+        System.out.println("2.PvP (Prize Table)");
+        System.out.println("3.Play vs AI (Easy mode)");
+        System.out.println("4.Play vs AI (Hard mode)");
+        System.out.println("5.Exit");
 
-        // Place ships randomly on each player's grid
-        placeShips(player1Grid);
-        placeShips(player2Grid);
+        switch (scanner.nextInt()) {
+            case 1: runNormal(); break;
+            case 2: runPrizeTable(); break;
+            case 3: runEasyAI(); break;
+            case 4: runHardAI(); break;
+            case 5: System.exit(0);
+        }
+    }
 
-        // Variable to track whose turn it is
+    static void runNormal(){
+        initializeGrids();
         boolean player1Turn = true;
 
-        // Main game loop, runs until one player's ships are all sunk
         while (!isGameOver()) {
             if (player1Turn) {
                 System.out.println("Player 1's turn:");
+                checkGrid(player2Grid);
                 printGrid(player1TrackingGrid);
                 playerTurn(player2Grid, player1TrackingGrid);
             } else {
                 System.out.println("Player 2's turn:");
+                checkGrid(player1Grid);
                 printGrid(player2TrackingGrid);
                 playerTurn(player1Grid, player2TrackingGrid);
             }
@@ -58,94 +80,304 @@ public class BattleShip {
         }
 
         System.out.println("Game Over!");
+        if (player1Turn) {
+            System.out.println("Player 2 won!");
+        }
+        else{
+            System.out.println("Player 1 won!");
+        }
     }
 
-    /**
-      Initializes the grid by filling it with water ('~').
+    static void runPrizeTable(){
+        initializeGrids();
+        boolean player1Turn = true;
+        while (!isGameOver()) {
+            if (player1Turn) {
+                System.out.println("Player 1's turn:");
+                checkGrid(player2Grid);
+                printGrid(player1TrackingGrid);
+                playerTurn(player2Grid, player1TrackingGrid);
+            }
+            else{
+                System.out.println("Player 2's turn:");
+                checkGrid(player1Grid);
+                printGrid(player2TrackingGrid);
+                playerTurn(player1Grid, player2TrackingGrid);
+            }
 
-      @param grid The grid to initialize.
-     */
+            if(!isHit) player1Turn = !player1Turn;
+
+        }
+        System.out.println("Game Over!");
+        if (player1Turn) {
+            System.out.println("Player 1 won!");
+        }
+        else{
+            System.out.println("Player 2 won!");
+        }
+    }
+
+    static void runEasyAI(){
+        initializeGrids();
+        boolean player1Turn = true;
+        while (!isGameOver()) {
+            if (player1Turn) {
+                System.out.println("Player 1's turn:");
+                checkGrid(player2Grid);
+                printGrid(player1TrackingGrid);
+                playerTurn(player2Grid, player1TrackingGrid);
+            }
+            else{
+                checkGrid(player1Grid);
+                printGrid(player2TrackingGrid);
+                makeEasyMoveAI(player1Grid, player2TrackingGrid);
+            }
+            player1Turn = !player1Turn;
+        }
+        System.out.println("Game Over!");
+        if (player1Turn) {
+            System.out.println("You lost!");
+        }
+        else{
+            System.out.println("You won!");
+        }
+    }
+
+    static void runHardAI(){
+        initializeGrids();
+        boolean player1Turn = true;
+        while (!isGameOver()) {
+            if (player1Turn) {
+                System.out.println("Player 1's turn:");
+                checkGrid(player2Grid);
+                printGrid(player1TrackingGrid);
+                playerTurn(player2Grid, player1TrackingGrid);
+            }
+            else{
+                checkGrid(player1Grid);
+                printGrid(player2TrackingGrid);
+                if(!isHit) makeEasyMoveAI(player1Grid, player2TrackingGrid);
+                else makeHardMoveAI(player1Grid, player2TrackingGrid);
+            }
+            player1Turn = !player1Turn;
+        }
+        System.out.println("Game Over!");
+        if (player1Turn) {
+            System.out.println("You lost!");
+        }
+        else{
+            System.out.println("You won!");
+        }
+    }
+
+    static List<int[]> hardMoveTargets = new ArrayList<>();
+
+    static void makeHardMoveAI(char[][] opponentGrid, char[][] trackingGrid) {
+        Random rand = new Random();
+        int[][] directions = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
+
+        if(hardMoveTargets.isEmpty()) {
+            for (int[] dir : directions) {
+                int newRow = lastHitRow + dir[0];
+                int newCol = lastHitCol + dir[1];
+                if (newRow >= 0 && newRow < GRID_SIZE && newCol >= 0 && newCol < GRID_SIZE && (opponentGrid[newRow][newCol] == 's' || opponentGrid[newRow][newCol] == '~')) {
+                    hardMoveTargets.add(new int[]{newRow, newCol});
+                }
+            }
+        }
+
+        if(hardMoveTargets.isEmpty()) {
+            isHit = false;
+            makeEasyMoveAI(opponentGrid, trackingGrid);
+            return;
+        }
+
+        int randomHMove = rand.nextInt(hardMoveTargets.size());
+        int[] hardMove = hardMoveTargets.get(randomHMove);
+        int Row = hardMove[0];
+        int Col = hardMove[1];
+
+        if(opponentGrid[Row][Col] == 's') {
+            System.out.println("Did Hit!\n");
+            opponentGrid[Row][Col] = 'x';
+            trackingGrid[Row][Col] = 'x';
+            isHit = true;
+            lastHitRow = Row;
+            lastHitCol = Col;
+            hardMoveTargets.clear();
+            for (int[] dir : directions) {
+                int newRow = lastHitRow + dir[0];
+                int newCol = lastHitCol + dir[1];
+                if (newRow >= 0 && newRow < GRID_SIZE && newCol >= 0 && newCol < GRID_SIZE && (opponentGrid[newRow][newCol] == 's' || opponentGrid[newRow][newCol] == '~')) {
+                    hardMoveTargets.add(new int[]{newRow, newCol});
+                }
+            }
+        }else{
+            System.out.println("Did Miss!\n");
+            opponentGrid[Row][Col] = '0';
+            trackingGrid[Row][Col] = '0';
+            hardMoveTargets.remove(hardMove);
+        }
+    }
+
+    static void makeEasyMoveAI(char[][] opponentGrid, char[][] trackingGrid){
+        isHit = false;
+        int Row;
+        int Col;
+        do{
+            Random rand = new Random();
+            Row = rand.nextInt(opponentGrid.length);
+            Col = rand.nextInt(opponentGrid.length);
+        }while(opponentGrid[Row][Col] != '~');
+
+        if(opponentGrid[(Row)][Col] == 's'){
+            System.out.println("Did Hit!\n");
+            opponentGrid[(Row)][Col] = 'x';
+            trackingGrid[(Row)][Col] = 'x';
+            isHit = true;
+            lastHitRow = Row;
+            lastHitCol = Col;
+        }
+        if(opponentGrid[(Row)][Col] == '~'){
+            System.out.println("Did Miss!\n");
+            opponentGrid[(Row)][Col] = '0';
+            trackingGrid[(Row)][Col] = '0';
+        }
+    }
+
     static void initializeGrid(char[][] grid) {
-        //todo
+        for(int x = 0; x < GRID_SIZE; x++){
+            for(int y = 0; y < GRID_SIZE; y++){
+                grid[x][y] = '~';
+            }
+        }
     }
 
-    /**
-      Places ships randomly on the given grid.
-      This method is called for both players to place their ships on their respective grids.
+    static void initializeGrids() {
+        initializeGrid(player1Grid);
+        initializeGrid(player2Grid);
+        initializeGrid(player1TrackingGrid);
+        initializeGrid(player2TrackingGrid);
 
-      @param grid The grid where ships need to be placed.
-     */
+        placeShips(player1Grid);
+        placeShips(player2Grid);
+    }
+
     static void placeShips(char[][] grid) {
-        //todo
+        for(int x=5; x>=2; x--){
+            do{
+                Random rand = new Random();
+                row = rand.nextInt(GRID_SIZE);
+                col = rand.nextInt(GRID_SIZE);
+                size = x;
+                horizontal = rand.nextBoolean();
+            } while(!canPlaceShip(grid ,row ,col ,size ,horizontal));
+
+            for(int i=0; i<size; i++){
+                if(horizontal){
+                    grid[row][col+i] = 's';
+                }
+                else{
+                    grid[row+i][col] = 's';
+                }
+            }
+        }
     }
 
-    /**
-      Checks if a ship can be placed at the specified location on the grid.
-      This includes checking the size of the ship, its direction (horizontal or vertical),
-      and if there's enough space to place it.
-
-      @param grid The grid where the ship is to be placed.
-      @param row The starting row for the ship.
-      @param col The starting column for the ship.
-      @param size The size of the ship.
-      @param horizontal The direction of the ship (horizontal or vertical).
-      @return true if the ship can be placed at the specified location, false otherwise.
-     */
     static boolean canPlaceShip(char[][] grid, int row, int col, int size, boolean horizontal) {
-        //todo
+        if(horizontal){
+            if(col + size > grid.length) return false;
+            for(int x=0; x<size; x++){
+                int col_temp = col + x;
+                if(grid[row][col_temp] != '~') return false;
+            }
+        }else{
+            if(row + size > grid.length) return false;
+            for(int x=0; x<size; x++){
+                int row_temp = row + x;
+                if(grid[row_temp][col] != '~') return false;
+            }
+        }
         return true;
     }
 
-    /**
-      Manages a player's turn, allowing them to attack the opponent's grid
-      and updates their tracking grid with hits or misses.
-
-      @param opponentGrid The opponent's grid to attack.
-      @param trackingGrid The player's tracking grid to update.
-     */
     static void playerTurn(char[][] opponentGrid, char[][] trackingGrid) {
-        //todo
+        isHit = false;
+        while(true){
+            String input = scanner.next();
+            if(!isValidInput(input)) {
+                System.out.println("Invalid input! Try again!");
+                continue;
+            }
+            char colChar = input.charAt(0);
+            int Row = Character.getNumericValue(input.charAt(1));
+            int Col = (int) (colChar - 'A');
+            if(opponentGrid[(Row)][Col] == 'x' || opponentGrid[(Row)][Col] == '0'){
+                System.out.println("Invalid move! Try again!");
+                continue;
+            }
+            if(opponentGrid[(Row)][Col] == 's'){
+                System.out.println("You Hit!\n");
+                opponentGrid[(Row)][Col] = 'x';
+                trackingGrid[(Row)][Col] = 'x';
+                isHit = true;
+                break;
+            }
+            if(opponentGrid[(Row)][Col] == '~'){
+                System.out.println("You Miss!\n");
+                opponentGrid[(Row)][Col] = '0';
+                trackingGrid[(Row)][Col] = '0';
+                break;
+            }
+        }
     }
 
-    /**
-      Checks if the game is over by verifying if all ships are sunk.
+    static void checkGrid(char[][] grid) {
+        remainParts = 0;
+        hits = 0;
+        misses = 0;
+        for(int x=0; x<GRID_SIZE; x++){
+            for(int y=0; y<GRID_SIZE; y++){
+                if(grid[x][y] == 's') remainParts++;
+                if(grid[x][y] == 'x') hits++;
+                if(grid[x][y] == '0') misses++;
+            }
+        }
+        totalShots = hits + misses;
+    }
 
-      @return true if the game is over (all ships are sunk), false otherwise.
-     */
     static boolean isGameOver() {
-        //todo
-        return false;
+        return allShipsSunk(player1Grid) || allShipsSunk(player2Grid);
     }
 
-    /**
-      Checks if all ships have been destroyed on a given grid.
-
-      @param grid The grid to check for destroyed ships.
-      @return true if all ships are sunk, false otherwise.
-     */
     static boolean allShipsSunk(char[][] grid) {
-        //todo
+        for(int x=0; x<GRID_SIZE; x++){
+            for(int y=0; y<GRID_SIZE; y++){
+                if(grid[x][y] == 's') return false;
+            }
+        }
         return true;
     }
 
-    /**
-      Validates if the user input is in the correct format (e.g., A5).
-
-      @param input The input string to validate.
-      @return true if the input is in the correct format, false otherwise.
-     */
     static boolean isValidInput(String input) {
-        //todo
-        return true;
+        if(input.isEmpty()) return false;
+        if(input.length() != 2) return false;
+        return Character.isLetter(input.charAt(0)) && Character.isDigit(input.charAt(1));
     }
 
-    /**
-      Prints the current state of the player's tracking grid.
-      This method displays the grid, showing hits, misses, and untried locations.
-
-      @param grid The tracking grid to print.
-     */
     static void printGrid(char[][] grid) {
-        //todo
+        System.out.print("  ");
+        for(int x=0; x<GRID_SIZE; x++){
+            System.out.print((char)(x + 65) + " ");
+        }
+        System.out.println();
+        for(int x=0; x<GRID_SIZE; x++){
+            System.out.print(x + " ");
+            for(int y=0; y<GRID_SIZE; y++){
+                System.out.print(grid[x][y] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Current table stats: hits: " + hits + " totalShots: " + totalShots + " remainParts: " + remainParts);
     }
 }
